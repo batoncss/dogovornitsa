73cedb9d3c7735bfb8f3d4b885addc4d03b3cc25
batoncss <batoncss@yandex.ru>
2024-12-13 15:34:13 +0300

    update form for participant added form for watching and deleting participant

--- Dogovornitsa/apps/contracts/urls.py
+++ Dogovornitsa/apps/contracts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import participants
+from .views import participants_add, participants_list, participants_delete
 
 
 urlpatterns = [
-    path('participants', participants, name='participants'),
+    path('participants-add', participants_add, name='participants-add'),
+    path('participants-list', participants_list, name='participants-list'),
+    path('participants/delete/<int:participant_id>/', participants_delete, name='participants_delete'),
 ]
--- Dogovornitsa/apps/contracts/views.py
+++ Dogovornitsa/apps/contracts/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from Dogovornitsa.apps.contracts.forms import ParticipantForm
 from Dogovornitsa.apps.contracts.models import Participant
 
 
-def participants(request):
+def participants_add(request):
     if request.method == 'POST':
         form = ParticipantForm(request.POST)
         if form.is_valid():
             form.save()  # Сохраняем новую запись
-            return redirect('participants')  # Перезагрузка страницы
+            return redirect('participants-add')  # Перезагрузка страницы
     else:
         form = ParticipantForm()
     all_participants = Participant.objects.all()
-    return render(request, "pages/contracts/participants.html", {"participants": all_participants, 'form': form})
+    return render(request, "pages/contracts/participants-add.html", {"participants": all_participants, 'form': form})
+
+def participants_list(request):
+    all_participants = Participant.objects.all()
+    return  render(request, "pages/contracts/participants-list.html", {"participants": all_participants})
+
+def participants_delete(request, participant_id):
+    participant = get_object_or_404(Participant, id=participant_id)
+    participant.delete()
+    return redirect('participants-list')  # Перезагрузка страницы
